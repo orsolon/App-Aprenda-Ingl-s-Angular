d92b1305486a128fa701af3bd22889b3cf762f19
orsolon <56239735+orsolon@users.noreply.github.com>
2021-03-04 14:24:10 -0300

    68 – Criando a classe coração

--- src/app/tentativas/tentativas.component.ts
+++ src/app/tentativas/tentativas.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
-@Component({
+import { Coracao } from '../shared/coracao.model';
+
+ @Component({
   selector: 'app-tentativas',
   templateUrl: './tentativas.component.html',
   styleUrls: ['./tentativas.component.css']
 })
 export class TentativasComponent implements OnInit {
+  
   public coracaoVazio: string = "/assets/coracao_vazio.png"
   public coracaoCheio: string = "/assets/coracao_cheio.png"
+
+  public coracoes: Coracao[] = [
+    new Coracao(true), new Coracao(true), new Coracao(true)
+  ]
   
-  constructor() { }
+  constructor() { 
+    console.log(this.coracoes)
+  }
 
   ngOnInit(): void {
   }
