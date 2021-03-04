7b0bd67e77b6897d348d5a82e5fdb802b81f032e
orsolon <56239735+orsolon@users.noreply.github.com>
2021-03-04 13:49:39 -0300

    66-PropertyBinding @Input

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -12,14 +12,41 @@ export class PainelComponent implements OnInit {
 
   public frases: Frase[] = FRASES
   public instrucao: string = "Traduza a frase:"
+  public resposta: string
 
-  constructor() { console.log(this.frases) }
+  //se o usuario acertar vai para fase seguinte e cada rodada vai abrir uma nova frase
+  public rodada: number = 0 //vai de 0 até array.length
+  public rodadaFrase: Frase //exibe a frase da rodada
 
-  ngOnInit(): void {
+  public progresso: number = 0
+  
+  constructor() { 
+    this.rodadaFrase = this.frases[this.rodada]
+    console.log(this.rodadaFrase) }
+
+  ngOnInit(): void { 
   }
 
-  public atualizaResposta(): void {
-    console.log('teste')
+  public atualizaResposta(resposta: Event): void {
+    this.resposta = (<HTMLInputElement>resposta.target).value
+    // console.log(this.resposta)
+  }
+
+  public verificarResposta(): void {
+    if(this.rodadaFrase.frasePtBr == this.resposta){
+      alert('A traducao esta correta')
+      //trocar a pergunta da rodada
+        this.rodada++
+      //progresso
+      this.progresso = this.progresso + (100 / this.frases.length)
+      console.log(this.progresso)
+      //atualiza o objeto frase da rodada
+        this.rodadaFrase = this.frases[this.rodada]
+    } else {
+      alert('A traducão está errada')
+    }
+    
+    
   }
 
 }
