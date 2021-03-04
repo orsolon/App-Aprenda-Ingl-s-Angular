02a4fe0fa80276b7b7dcda970fe89fba9046076f
orsolon <56239735+orsolon@users.noreply.github.com>
2021-03-04 14:06:21 -0300

    67 – Atualizando a resposta (limpar o input)

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -12,7 +12,7 @@ export class PainelComponent implements OnInit {
 
   public frases: Frase[] = FRASES
   public instrucao: string = "Traduza a frase:"
-  public resposta: string
+  public resposta: string = ''
 
   //se o usuario acertar vai para fase seguinte e cada rodada vai abrir uma nova frase
   public rodada: number = 0 //vai de 0 até array.length
@@ -21,8 +21,8 @@ export class PainelComponent implements OnInit {
   public progresso: number = 0
   
   constructor() { 
-    this.rodadaFrase = this.frases[this.rodada]
-    console.log(this.rodadaFrase) }
+    this.atualizaRodada()
+  }
 
   ngOnInit(): void { 
   }
@@ -41,12 +41,20 @@ export class PainelComponent implements OnInit {
       this.progresso = this.progresso + (100 / this.frases.length)
       console.log(this.progresso)
       //atualiza o objeto frase da rodada
-        this.rodadaFrase = this.frases[this.rodada]
+      this.atualizaRodada()
+      
     } else {
       alert('A traducão está errada')
     }
     
     
+  }
+
+  public atualizaRodada(): void {
+    //define a frase da rodada com base em alguma logica
+    this.rodadaFrase = this.frases[this.rodada]
+    //limpa a resposta
+    this.resposta = " "
   }
 
 }
